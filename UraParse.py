a9f0a4aa65ddf7c36a45f5350b1b62424dcb7629
TRyc3 <84289295+TRyc3@users.noreply.github.com>
2022-01-26 17:45:13 -1000

    Add files via upload

---
+++ UraParse.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+import urllib.request
+import string
+import os.path
+from bs4 import BeautifulSoup as bs
+import re
+
+layout = [
+        [sg.Text("Paste Text"), 
+        sg.InputText(key = "Text")],
+
+        [sg.Text("Folder"),
+        sg.InputText(key = "Folder"),
+        sg.FolderBrowse()],
+
+        [sg.Button("Parse"),
+         sg.Button("Done")]
+    ]
+    
+window = sg.Window("Parser", layout)
+while True:
+    event,values = window.read()
+    if event == sg.WIN_CLOSED or event == "Done":
+        break
+
+    if event == "Parse":
+        try:
+            request_url = urllib.request.urlopen(values["Text"])
+            SrcCode = request_url.read()
+            SrcCode = SrcCode.decode('utf8')
+
+
+            
+
+            soup = bs(SrcCode, 'html.parser')
+
+            img = soup.find_all("script", string = re.compile('https://urasunday.com/secure/'))  
+
+            Target = "https://urasunday.com/secure/"
+            lines = Target[0].split()
+            # lines = SrcCode.split()
+            # urlList = []             
+
+            # for x in lines:
+            #     if Target in x:
+            #         bound = "'"
+            #         if x.find(bound) != x.rfind(bound):
+            #             urlList.append(x[x.find(bound) + 1: x.rfind(bound)])
+
+            count = 1
+            for x in urlList:
+                folder = values["Folder"]
+                try:
+                    SaveAs = os.path.join(folder, str(count) + ".webp")
+                    urllib.request.urlretrieve(x, SaveAs)
+                    count += 1
+                except Exception as e :
+                    sg.popup(str(e))
+                    pass
+
+            sg.popup("Done")
+
+        except Exception as e :
+            sg.popup(str(e))
+
+window.close()
